3dc19c35b6a3d6d7b11e6e6027693317f94da6ca
nagaohiroki <testplayers0226@gmail.com>
2025-04-02 12:32:30 +0900

    Added environment variables before starting the process. Allows the use of any client. #2

--- NeovimForUnity/Assets/Packages/Editor/NeovimEditor.cs
+++ NeovimForUnity/Assets/Packages/Editor/NeovimEditor.cs
@@ -1,27 +1,34 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Collections;
 using UnityEditor;
 using UnityEngine;
 using Unity.CodeEditor;
+#if UNITY_EDITOR_OSX
+using System.Xml.Linq;
+using System.Linq;
+#endif
 namespace NeovimEditor
 {
 	[InitializeOnLoad]
 	public class NeovimEditor : IExternalCodeEditor
 	{
-		const string nvimName = "nvim";
-		const string keyNvimCmd = "nvim_cmd";
 		const string keyNvimOverrideCmd = "nvim_override_cmd";
 		const string keyNvimArgs = "nvim_args";
 		const string keyNvimExt = "nvim_ext";
+		const string keyNvimKeywords = "nvim_cmd_keywords";
 		const string defaultExt = ".cs,.shader,.json,.xml,.txt,.yml,.yaml,.md";
 		const string defaultArgs = "+$(Line) \"$(File)\"";
+		const string defaultNvimKeywords = "nvim,neovide";
 		string[] Extensions => GetString(keyNvimExt, defaultExt).Split(',');
+		string[] Keywords => GetString(keyNvimKeywords, defaultNvimKeywords).Split(',');
 		public static bool IsEnabled => CodeEditor.CurrentEditor is NeovimEditor;
 		public CodeEditor.Installation[] Installations => new[]
 		{
 			new CodeEditor.Installation
 			{
-				Name = nvimName,
-				Path = EditorPrefs.GetString(keyNvimCmd)
+				Name = "nvim",
+				Path = ""
 			}
 		};
 		static NeovimEditor()
@@ -35,14 +42,16 @@ namespace NeovimEditor
 		}
 		public void OnGUI()
 		{
+			var path = CodeEditor.Editor.CurrentInstallation.Path;
 			EditorGUILayout.BeginVertical();
-			TextField($"Command(empty:{GetNvimExe()})", keyNvimOverrideCmd, string.Empty);
+			TextField($"Command(empty:{GetNvimExe(path)})", keyNvimOverrideCmd, string.Empty);
 			TextField("Arguments", keyNvimArgs, defaultArgs);
 			TextField("Extensions", keyNvimExt, defaultExt);
+			NeovimCommandGUI();
 			EditorGUILayout.EndVertical();
 			CodeEditor.SetExternalScriptEditor(FindVSPath());
 			CodeEditor.Editor.CurrentCodeEditor.OnGUI();
-			CodeEditor.SetExternalScriptEditor(EditorPrefs.GetString(keyNvimCmd));
+			CodeEditor.SetExternalScriptEditor(path);
 		}
 		public bool OpenProject(string filePath, int line, int column)
 		{
@@ -57,7 +66,7 @@ namespace NeovimEditor
 			var exe = EditorPrefs.GetString(keyNvimOverrideCmd);
 			if(string.IsNullOrEmpty(exe))
 			{
-				exe = GetNvimExe();
+				exe = GetNvimExe(CodeEditor.Editor.CurrentInstallation.Path);
 			}
 			var info = new System.Diagnostics.ProcessStartInfo
 			{
@@ -66,6 +75,11 @@ namespace NeovimEditor
 				CreateNoWindow = false,
 				UseShellExecute = false,
 			};
+			var envs = Environment.GetEnvironmentVariables();
+			foreach(DictionaryEntry env in envs)
+			{
+				info.EnvironmentVariables[(string)env.Key] = (string)env.Value;
+			}
 			System.Diagnostics.Process.Start(info);
 			return true;
 		}
@@ -83,14 +97,13 @@ namespace NeovimEditor
 		}
 		public bool TryGetInstallationForPath(string editorPath, out CodeEditor.Installation installation)
 		{
-			if(editorPath.Contains("nvim"))
+			if(IsNvimPath(editorPath))
 			{
 				installation = new()
 				{
-					Name = nvimName,
+					Name = Path.GetFileNameWithoutExtension(editorPath),
 					Path = editorPath
 				};
-				EditorPrefs.SetString(keyNvimCmd, editorPath);
 				return true;
 			}
 			installation = default;
@@ -104,9 +117,10 @@ namespace NeovimEditor
 				Debug.Log("Visual Studio is not found");
 				return;
 			}
+			var path = CodeEditor.Editor.CurrentInstallation.Path;
 			CodeEditor.SetExternalScriptEditor(vs);
 			CodeEditor.Editor.CurrentCodeEditor.SyncAll();
-			CodeEditor.SetExternalScriptEditor(EditorPrefs.GetString(keyNvimCmd));
+			CodeEditor.SetExternalScriptEditor(path);
 		}
 		bool IsCodeAssets(string[] files, string[] ext)
 		{
@@ -131,23 +145,22 @@ namespace NeovimEditor
 			}
 			return false;
 		}
-		string GetString(string inKey, string inDefault)
+		string GetNvimExe(string inPath)
 		{
-			var text = EditorPrefs.GetString(inKey);
-			if(!string.IsNullOrEmpty(text))
+#if UNITY_EDITOR_OSX
+			if(inPath.EndsWith(".app"))
 			{
-				return text;
+				return FindExecutableMacOS(inPath);
 			}
-			return inDefault;
+#endif
+			return inPath;
 		}
-		string GetNvimExe()
+		void NeovimCommandGUI()
 		{
-			var exe = EditorPrefs.GetString(keyNvimCmd);
-			if(exe.EndsWith(".app"))
-			{
-				return Path.Combine(exe, "Contents", "MacOS", Path.GetFileNameWithoutExtension(exe));
-			}
-			return exe;
+			EditorGUILayout.BeginHorizontal();
+			EditorGUILayout.LabelField("Neovim Command (Configure on Preferences > Neovim Settings)");
+			EditorGUILayout.LabelField(EditorPrefs.GetString(keyNvimKeywords));
+			EditorGUILayout.EndHorizontal();
 		}
 		void TextField(string inLabel, string inKey, string inDefaultValue)
 		{
@@ -168,5 +181,70 @@ namespace NeovimEditor
 			}
 			return null;
 		}
+		bool IsNvimPath(string inPath)
+		{
+			var appName = Path.GetFileNameWithoutExtension(inPath).ToLower();
+			var keywords = Keywords;
+			foreach(var name in keywords)
+			{
+				if(appName.Contains(name.ToLower()))
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+#if UNITY_EDITOR_OSX
+		string FindExecutableMacOS(string inPath)
+		{
+			var contents = Path.Combine(inPath, "Contents");
+			var infoPlistPath = Path.Combine(contents, "Info.plist");
+			if(File.Exists(infoPlistPath))
+			{
+				try
+				{
+					var doc = XDocument.Load(infoPlistPath);
+					var executableElement = doc.Root.Element("dict").Elements("key").FirstOrDefault(e => e.Value == "CFBundleExecutable");
+					if(executableElement != null)
+					{
+						var executableName = executableElement.ElementsAfterSelf("string").First().Value;
+						var contentsPath = Path.Combine(contents, "MacOS");
+						return Path.Combine(contentsPath, executableName);
+					}
+				}
+				catch(System.Exception e)
+				{
+					Debug.LogError("Error parsing Info.plist: " + e.Message);
+				}
+			}
+			return null;
+		}
+#endif
+		[SettingsProvider]
+		public static SettingsProvider CreateProjectSettingMenu()
+		{
+			return new SettingsProvider("Preferences/Neovim Settings", SettingsScope.User)
+			{
+				label = "Neovim Settings",
+				guiHandler = Gui
+			};
+		}
+		static void Gui(string inSearchContext)
+		{
+			EditorGUILayout.BeginVertical();
+			EditorGUILayout.LabelField("Neovim Command Keywords(Application Keywords (comma-separated, partial matches allowed))");
+			EditorPrefs.SetString(keyNvimKeywords, EditorGUILayout.TextField(GetString(keyNvimKeywords, defaultNvimKeywords)));
+			EditorGUILayout.EndHorizontal();
+		}
+		static string GetString(string inKey, string inDefault)
+		{
+			var text = EditorPrefs.GetString(inKey);
+			if(!string.IsNullOrEmpty(text))
+			{
+				return text;
+			}
+			return inDefault;
+		}
 	}
+
 }
